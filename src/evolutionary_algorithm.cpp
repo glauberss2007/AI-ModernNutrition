02dfd8d9a6690630426fd3c25f94964e1c561b68
Glauber Soares <glauberss2007@gmail.com>
2021-08-08 06:35:40 -0300

    Update evolutionary_algorithm.cpp

--- src/evolutionary_algorithm.cpp
+++ src/evolutionary_algorithm.cpp
@@ -55,9 +55,9 @@ void evolutionary_algorithm::algorithm() {
     this->_competition_between_parents_and_children = false;
     this->_elitism_proportion = 0.02;
     this->_reproduction_scaling_strategy = scaling_strategy::window;
-    this->_reproduction_selection_strategy = selection_strategy::uniform;
+    this->_reproduction_selection_strategy = selection_strategy::truncate;
     this->_survival_scaling_strategy = scaling_strategy::window;
-    this->_survival_selection_strategy = selection_strategy::uniform;
+    this->_survival_selection_strategy = selection_strategy::truncate;
 }
 
 void evolutionary_algorithm::initialize_population() {
